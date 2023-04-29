ca1e4d692718ee7e8b67e1cb635e7adfac8b3b56
bjarkt <688414+bjarkt@users.noreply.github.com>
2023-04-29 12:41:16 +0200

    js test

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  public readonly currentTime = interval(1000).pipe(map(() => new Date()));
 }
