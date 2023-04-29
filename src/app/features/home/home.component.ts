0e294117c8809a74ed2f98127d8e3f5736fbae36
bjarkt <688414+bjarkt@users.noreply.github.com>
2023-04-29 13:03:34 +0200

    timer

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,6 +1,6 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
-import { BehaviorSubject, Observable, interval, map } from 'rxjs';
+import { BehaviorSubject, Observable, interval, map, timer } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +8,12 @@ import { BehaviorSubject, Observable, interval, map } from 'rxjs';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  public readonly currentTime: Observable<Date>;
+  public readonly currentTime: Observable<Date> = new BehaviorSubject(
+    new Date()
+  );
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
     if (isPlatformBrowser(this.platformId)) {
-      this.currentTime = interval(1000).pipe(map(() => new Date()));
-    } else {
-      this.currentTime = new BehaviorSubject(new Date());
+      this.currentTime = timer(0, 1000).pipe(map(() => new Date()));
     }
   }
 }
