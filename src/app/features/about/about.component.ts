98aaae21c44d448e9b848d18e0b1e887af74cd3b
bjarkt <688414+bjarkt@users.noreply.github.com>
2023-06-10 15:23:32 +0200

    sse test

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, DestroyRef, signal, effect } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {}
+export class AboutComponent {
+  private readonly eventSource = new EventSource(
+    `${environment.apiUrl}/api/events/events`
+  );
+  public events = signal<string[]>([]);
+
+  constructor() {
+    inject(DestroyRef).onDestroy(() => {
+      this.eventSource.close();
+    });
+    this.eventSource.addEventListener('message', (event) => {
+      console.log(event);
+      this.events.mutate((events) => {
+        events.push(event.data);
+      });
+    });
+    this.eventSource.addEventListener('error', (event) => {
+      console.error(event);
+    });
+  }
+}
